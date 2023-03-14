ac66fe51772487f685ac7fd137ae9d5cc63497b7
zpyuan6 <zpyuan6@163.com>
2023-03-14 11:16:01 +0000

    Start training

--- train_model.py
+++ train_model.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
-import torch
-
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 if __name__ == "__main__":
     model = YOLO("yolov8n.pt")
--- transfor_annotation.py
+++ transfor_annotation.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import json
 import tqdm 
+import copy
 
 def generate_bounding_box_json():
     mode = ["test", "train", "val"]
@@ -69,11 +70,94 @@ def generate_annotation_txt():
             annotation_file.writelines(writing_content)
             annotation_file.close()
 
+def clean_annotation():
+    mode = ["test", "train", "val"]
 
-if __name__ == "__main__":
-    generate_bounding_box_json()
+    for m in mode:
+        img_dir = f"F:\\nematoda\\nemadote_detection\\images\\{m}"
+        label_dir = f"F:\\nematoda\\nemadote_detection\\labels\\{m}"
 
-    generate_annotation_txt()
 
+        image_id_list = []
+        label_id_list = []
+
+        for root, folders, files in os.walk(label_dir):
+            for index, file in enumerate(files):
+                label_id = file.split(".")[0]
+                label_id_list.append(label_id)
+               
+
+        for root, folders, files in os.walk(img_dir):
+            for file in files:
+                image_id_list.append(file.split(".")[0])
+                if file.split(".")[0] in label_id_list:
+                    s = True
+                else:
+                    print(m, file)
+        
+
+        print("len: ",m,len(label_id_list))
+        print(len(image_id_list))
+
+def clean_and_refactor():
+    # clean_annotation()
+    mode = ["test", "train", "val"]
+
+    for m in mode:
+        label_dir = f"F:\\nematoda\\nemadote_detection\\labels\\{m}"
+
+        for root, folders, files in os.walk(label_dir):
+            for file in files:
+                annotation = []
+                f = open(os.path.join(root,file),"r")
+                line = f.readline().split(" ")[1:]
+                f.close()
+                
+                index = int(len(line)/4)
+                print(index)
+                for i in range(index):
+                    a = line[i*4:(i+1)*4]
+                    s = "0 " + " ".join(a)
+                    if i != index-1:
+                        s = s+"\n"
+                    # print(s)
+                    annotation.append(s)  
+
+                f = open(os.path.join(root,file),"w")
+                f.writelines(annotation)
+                f.close()
+                
                 
 
+
+def check_txt_annotation(image_path, txt_annotation):
+
+    img = cv2.imread(image_path)
+    print(img.shape)
+    img_weight = img.shape[1]
+    img_height = img.shape[0]
+
+    with open(txt_annotation) as f:
+        lines = f.readlines()
+        print(len(lines))
+        for line in lines:
+            x,y,w,h = line.split(" ")[1:]
+            x1 = int((float(x) - (float(w)/2))*img_weight)
+            x2 = int((float(x) + (float(w)/2))*img_weight)
+            y1 = int((float(y) - (float(h)/2))*img_height)
+            y2 = int((float(y) + (float(h)/2))*img_height)
+            # img, (bbox.left, bbox.top), (bbox.right, bbox.bottom), The top left corner of the image is the source point, downwards y increasing, right x increasing
+            print(x1,y1,x2,y2)
+            img = cv2.rectangle(img, (x1,y1), (x2,y2), (0,255,0), 2)
+    
+    cv2.imshow("result",img)
+    cv2.waitKey(0)
+
+if __name__ == "__main__":
+    # generate_bounding_box_json()
+
+    # generate_annotation_txt()
+
+    check_txt_annotation("F:\\nematoda\\nemadote_detection\\images\\test\\img_038_id16.jpg","F:\\nematoda\\nemadote_detection\\labels\\test\\img_038_id16.txt")
+
+    # clean_and_refactor()
