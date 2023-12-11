b0fd20c5faf964efd260a9e6a9708b739c1a4894
zpyuan <m17603924166@gmail.com>
2023-12-11 20:38:08 +0000

    modify

--- train_model.py
+++ train_model.py
@@ -10,12 +10,12 @@ if __name__ == "__main__":
     model_list = ["yolov8n.pt","yolov8m.pt","yolov8x.pt"]
     # model_list = ["/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_tiny_07_06/weights/best.pt","/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_medium_07_06/weights/best.pt","/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_extra_07_06/weights/best.pt"]
 
-    model_large = YOLO("models_config\yolov8n_cbam_attention.yaml")
+    model_large = YOLO("models_config/yolov8n_cbam_attention.yaml")
     weight_model = torch.load(model_list[0])
     print(type(weight_model['model']))
     
     model_large.model.load(weight_model['model'])
-    # model_large.train(data="nematodes.yaml", epochs=400, imgsz=640, batch=32, name= "nematodes_tiny_11_Dec")
+    model_large.train(data="nematodes.yaml", epochs=400, imgsz=640, batch=32, name= "nematodes_tiny_attention_11_Dec")
 
     # model_large = YOLO(model_list[1])
     # model_large.train(data="nematodes.yaml", epochs=200, imgsz=640, batch=16, name= "nematodes_medium_31_08")
