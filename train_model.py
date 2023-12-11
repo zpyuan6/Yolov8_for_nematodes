5ed747ed67d81068abab3406ee374587ffaf6592
zpyuan <m17603924166@gmail.com>
2023-12-11 13:32:56 +0000

    modify

--- train_model.py
+++ train_model.py
@@ -6,14 +6,14 @@ os.environ['KMP_DUPLICATE_LIB_OK']='True'
 if __name__ == "__main__":
     # model = YOLO("yolov8m.pt")
     # model.train(data="nematodes.yaml", epochs=100, imgsz=640)
-    # model_list = ["yolov8n.pt","yolov8m.pt","yolov8x.pt"]
-    model_list = ["runs\\detect\\our_nemo_tiny_all2\\weights\\best.pt","runs\\detect\\our_nemo_medium_all\\weights\\best.pt","runs\\detect\\our_nemo_extra_all\\weights\\best.pt"]
+    model_list = ["yolov8n.pt","yolov8m.pt","yolov8x.pt"]
+    # model_list = ["/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_tiny_07_06/weights/best.pt","/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_medium_07_06/weights/best.pt","/home/zhipeng/Desktop/nametodes/Yolov8_for_nematodes/runs/detect/pest_uk_extra_07_06/weights/best.pt"]
 
     model_large = YOLO(model_list[0])
-    model_large.train(data="our_nematodes.yaml", epochs=200, imgsz=512, batch=48, name= "our_nemo_tiny_all_05_06")
+    model_large.train(data="nematodes.yaml", epochs=400, imgsz=640, batch=32, name= "nematodes_tiny_31_08")
 
-    # model_large = YOLO(model_list[1])
-    # model_large.train(data="our_nematodes.yaml", epochs=100, imgsz=640, batch=16, name= "our_nemo_medium_all_05_06")
+    model_large = YOLO(model_list[1])
+    model_large.train(data="nematodes.yaml", epochs=200, imgsz=640, batch=16, name= "nematodes_medium_31_08")
 
-    # model_large = YOLO(model_list[2])
-    # model_large.train(data="our_nematodes.yaml", epochs=50, imgsz=640, batch=2, name= "our_nemo_extra_all_05_06")
+    model_large = YOLO(model_list[2])
+    model_large.train(data="nematodes.yaml", epochs=200, imgsz=640, batch=2, name= "nematodes_extra_31_08")
